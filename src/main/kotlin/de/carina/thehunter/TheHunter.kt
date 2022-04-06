c21d9e750f5542cf7da9c5b78eb865e0f79a07eb
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-04-06 19:04:50 +0200

    recode commit

---
+++ src/main/kotlin/de/carina/thehunter/TheHunter.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright Notice for theHunterRemaster
+ * Copyright (c) at Carina Sophie Schoppe 2022
+ * File created on 06.04.22, 18:58 by Carina The Latest changes made by Carina on 06.04.22, 18:58 All contents of "TheHunter.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * at Carina Sophie Schoppe. All rights reserved
+ * Any type of duplication, distribution, rental, sale, award,
+ * Public accessibility or other use
+ * requires the express written consent of Carina Sophie Schoppe.
+ */
+
+package de.carina.thehunter
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TheHunter : JavaPlugin() {
+
+    /*
+    TODOS:
+    TODO: Add a config file
+    TODO: Add Messages File
+    TODO: MapLoader
+    TODO: GameStates
+    TODO: GameSigns
+    TODO: player Join event
+    TODO: player leave event
+    TODO: player death event
+    TODO: StatsSystem
+    TODO: MultiGameMode
+    TODO: CommandManager
+    TODO: InventoryBuilder
+    TODO: ItemBuilder
+    TODO: InventoryCommander
+    TODO: anvilGUI
+    TODO: Guns
+    TODO: ItemChests
+    TODO: Items for chests
+    TODO: DeathChests
+    TODO: Countdowns
+    TODO: API
+    TODO: Scoreboard
+    TODO: TeamMode
+    TODO: AutoUpdater
+    TODO: Map Reset
+    TODO: CleanUp
+     */
+
+}
