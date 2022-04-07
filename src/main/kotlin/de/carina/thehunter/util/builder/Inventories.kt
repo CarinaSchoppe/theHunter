20d346aa220065a027dcaa1b225d46001d24ae0b
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-04-07 23:42:43 +0200

    recode commit

--- src/main/kotlin/de/carina/thehunter/util/builder/Inventories.kt
+++ src/main/kotlin/de/carina/thehunter/util/builder/Inventories.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for theHunterRemaster
  * Copyright (c) at Carina Sophie Schoppe 2022
- * File created on 07.04.22, 23:39 by Carina The Latest changes made by Carina on 07.04.22, 23:39 All contents of "Inventories.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 07.04.22, 23:42 by Carina The Latest changes made by Carina on 07.04.22, 23:42 All contents of "Inventories.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at Carina Sophie Schoppe. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -10,7 +10,15 @@
 
 package de.carina.thehunter.util.builder
 
-object Inventories
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.Inventory
+
+object Inventories {
+    fun setupGameInventory(name: String): Inventory {
+        return InventoryBuilder("§d $name Game Setup", 18).fillInventory(ItemBuilder(Material.WHITE_STAINED_GLASS_PANE).addDisplayName("").addEnchantment(Enchantment.DURABILITY, 1).build()).create()
+    }
+}
 
 
 
