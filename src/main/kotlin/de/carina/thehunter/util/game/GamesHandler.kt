d0e94b67c1bf5c9356ba0f5ec04cc7fcb240c03b
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-04-16 11:27:40 +0200

    recode commit

--- src/main/kotlin/de/carina/thehunter/util/game/GamesHandler.kt
+++ src/main/kotlin/de/carina/thehunter/util/game/GamesHandler.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for theHunterRemaster
  * Copyright (c) at Carina Sophie Schoppe 2022
- * File created on 16.04.22, 11:27 by Carina The Latest changes made by Carina on 14.04.22, 00:35 All contents of "GamesHandler.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 16.04.22, 11:27 by Carina The Latest changes made by Carina on 16.04.22, 11:27 All contents of "GamesHandler.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at Carina Sophie Schoppe. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
