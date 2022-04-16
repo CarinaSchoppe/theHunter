9cec570d4a0bfb952a02a92bf803c6326a8c8568
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-04-16 12:02:41 +0200

    recode commit

--- src/main/kotlin/de/carina/thehunter/util/game/Scoreboard.kt
+++ src/main/kotlin/de/carina/thehunter/util/game/Scoreboard.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for theHunterRemaster
  * Copyright (c) at Carina Sophie Schoppe 2022
- * File created on 16.04.22, 12:02 by Carina The Latest changes made by Carina on 16.04.22, 12:01 All contents of "Scoreboard.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 16.04.22, 12:02 by Carina The Latest changes made by Carina on 16.04.22, 12:02 All contents of "Scoreboard.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at Carina Sophie Schoppe. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
