97e0635a052e7c3a41a9bb66814a8d9f81554d43
PixelsDE <carina.schoppe2912@gmail.com>
2022-04-18 19:10:16 +0200

    recode remaster

--- src/main/kotlin/de/carina/thehunter/util/misc/DeathChests.kt
+++ src/main/kotlin/de/carina/thehunter/util/misc/DeathChests.kt
@@ -3,6 +3,7 @@ package de.carina.thehunter.util.misc
 import de.carina.thehunter.util.game.Game
 import org.bukkit.Bukkit
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
 
@@ -20,9 +21,9 @@ class DeathChests(private val game: Game) {
         }
 
         player.location.block.type = org.bukkit.Material.CHEST
-
-        val location = player.location
-        chests[location] = inventory
+        val blockString = Material.AIR.toString() + ":" + player.location.world.name + ":" + player.location.x + ":" + player.location.y + ":" + player.location.z
+        game.mapResetter.blocks.add(blockString)
+        chests[player.location] = inventory
     }
 
 }
