6119633d1e231f628463f20b3c25f246ca246765
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-04-19 10:48:02 +0200

    recode commit

--- src/main/kotlin/de/carina/thehunter/events/game/PlayerKillsOther.kt
+++ src/main/kotlin/de/carina/thehunter/events/game/PlayerKillsOther.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for theHunterRemaster
  * Copyright (c) at Carina Sophie Schoppe 2022
- * File created on 19.04.22, 10:46 by Carina The Latest changes made by Carina on 19.04.22, 10:46 All contents of "PlayerKillsOther.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 19.04.22, 10:47 by Carina The Latest changes made by Carina on 19.04.22, 10:47 All contents of "PlayerKillsOther.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at Carina Sophie Schoppe. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -12,6 +12,7 @@ package de.carina.thehunter.events.game
 
 import de.carina.thehunter.TheHunter
 import de.carina.thehunter.gamestates.IngameState
+import de.carina.thehunter.util.game.Game
 import de.carina.thehunter.util.game.GamesHandler
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
@@ -45,20 +46,16 @@ class PlayerKillsOther : Listener {
         if (game.currentGameState !is IngameState)
             return
         TheHunter.instance.statsSystem.playerKilledOtherPlayer(killer, player)
-        game.deathChests.createDeathChest(player)
-        game.players.remove(player)
-        game.spectators.add(player)
-        player.teleport(game.spectatorLocation!!)
-        for (players in Bukkit.getOnlinePlayers()) {
-            players.showPlayer(TheHunter.instance, player)
-        }
+        generalHandling(player, game)
         game.players.forEach {
             it.sendMessage(TheHunter.instance.messages.messagesMap["player-killed-by-other"]!!.replace("%player%", player.name).replace("%killer%", killer.name))
             it.hidePlayer(TheHunter.instance, player)
+            player.hidePlayer(TheHunter.instance, it)
         }
         game.spectators.filter { it.name != player.name }.forEach {
             it.sendMessage(TheHunter.instance.messages.messagesMap["player-killed-by-other"]!!.replace("%player%", player.name).replace("%killer%", killer.name))
             it.hidePlayer(TheHunter.instance, player)
+            player.hidePlayer(TheHunter.instance, it)
         }
         player.sendMessage(TheHunter.instance.messages.messagesMap["player-own-killed-by-other"]!!.replace("%killer%", killer.name))
         if (game.checkWinning())
@@ -72,6 +69,24 @@ class PlayerKillsOther : Listener {
         if (game.currentGameState !is IngameState)
             return
         TheHunter.instance.statsSystem.playerDied(player)
+        generalHandling(player, game)
+        game.players.forEach {
+            it.sendMessage(TheHunter.instance.messages.messagesMap["player-died"]!!.replace("%player%", player.name))
+            it.hidePlayer(TheHunter.instance, player)
+            player.hidePlayer(TheHunter.instance, it)
+        }
+        game.spectators.filter { it.name != player.name }.forEach {
+            it.sendMessage(TheHunter.instance.messages.messagesMap["player-died"]!!.replace("%player%", player.name))
+            it.hidePlayer(TheHunter.instance, player)
+            player.hidePlayer(TheHunter.instance, it)
+        }
+        player.sendMessage(TheHunter.instance.messages.messagesMap["player-own-died"]!!)
+        if (game.checkWinning())
+            game.nextGameState()
+
+    }
+
+    private fun generalHandling(player: Player, game: Game) {
         game.deathChests.createDeathChest(player)
         game.players.remove(player)
         game.spectators.add(player)
@@ -79,17 +94,5 @@ class PlayerKillsOther : Listener {
         for (players in Bukkit.getOnlinePlayers()) {
             players.showPlayer(TheHunter.instance, player)
         }
-        game.players.forEach {
-            it.sendMessage(TheHunter.instance.messages.messagesMap["player-died"]!!.replace("%player%", player.name))
-            it.hidePlayer(TheHunter.instance, player)
-        }
-        game.spectators.filter { it.name != player.name }.forEach {
-            it.sendMessage(TheHunter.instance.messages.messagesMap["player-died"]!!.replace("%player%", player.name))
-            it.hidePlayer(TheHunter.instance, player)
-        }
-        player.sendMessage(TheHunter.instance.messages.messagesMap["player-own-died"]!!)
-        if (game.checkWinning())
-            game.nextGameState()
-
     }
 }
