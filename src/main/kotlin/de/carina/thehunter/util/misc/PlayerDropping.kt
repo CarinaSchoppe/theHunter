847a998c1aa6da5ef91307dd709a98e155f8a947
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-04-19 19:56:28 +0200

    recode commit

--- src/main/kotlin/de/carina/thehunter/util/misc/PlayerDropping.kt
+++ src/main/kotlin/de/carina/thehunter/util/misc/PlayerDropping.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for theHunterRemaster
  * Copyright (c) at Carina Sophie Schoppe 2022
- * File created on 19.04.22, 19:46 by Carina The Latest changes made by Carina on 19.04.22, 19:46 All contents of "PlayerDropping.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 19.04.22, 19:55 by Carina The Latest changes made by Carina on 19.04.22, 19:55 All contents of "PlayerDropping.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at Carina Sophie Schoppe. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
