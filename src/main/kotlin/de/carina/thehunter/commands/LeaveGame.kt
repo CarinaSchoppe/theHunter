2242b16570ebabfd21a25824e1db6aa34f211776
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-04-21 15:31:55 +0200

    recode commit

--- src/main/kotlin/de/carina/thehunter/commands/LeaveGame.kt
+++ src/main/kotlin/de/carina/thehunter/commands/LeaveGame.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for theHunterRemaster
  * Copyright (c) at Carina Sophie Schoppe 2022
- * File created on 21.04.22, 15:20 by Carina The Latest changes made by Carina on 21.04.22, 15:20 All contents of "LeaveGame.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 21.04.22, 15:26 by Carina The Latest changes made by Carina on 21.04.22, 15:26 All contents of "LeaveGame.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at Carina Sophie Schoppe. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
